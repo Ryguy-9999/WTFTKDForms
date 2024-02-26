52196c081f05600eb997a840f15b41587360cbc7
Ryan Cirincione <ryancirincione@aol.com>
2024-02-25 20:45:13 -0500

    major changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function canvas_arrow(context, fromx, fromy, tox, toy) {
     context.beginPath();
-    var headlen = 10; // length of head in pixels
+    var headlen = Math.sqrt(Math.pow(tox - fromx, 2) + Math.pow(toy - fromy, 2)) / 10; // length of head in pixels
     var dx = tox - fromx;
     var dy = toy - fromy;
     var angle = Math.atan2(dy, dx);
@@ -44,10 +44,22 @@ function draw_form(form) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     form.pattern.forEach((movement, index) => {
-        ctx.strokeStyle = (stanceColorData.hasOwnProperty(movement.stance) ? stanceColorData[movement.stance] : stanceColorData.default).replace('$alpha', index === currentStep ? '1' : '0.1');
         let newPoint = { x: currentPoint.x + movement.moveX * stepDistance, y: currentPoint.y + movement.moveY * stepDistance };
+        let colorAlpha = index === currentStep ? 1 : 0.1;
 
+        if(movement.kihup) {
+            ctx.fillStyle = `rgba(225,225,0,${colorAlpha})`;
+            ctx.beginPath();
+            ctx.arc(newPoint.x, newPoint.y, stepDistance / 8, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+
+        ctx.strokeStyle = (stanceColorData.hasOwnProperty(movement.stance) ? stanceColorData[movement.stance] : stanceColorData.default).replace('$alpha', ''+colorAlpha);
         canvas_arrow(ctx, currentPoint.x, currentPoint.y, newPoint.x, newPoint.y);
+
+        for(let i = 0; i < (movement.multiMove || 1); i++) {
+            canvas_arrow(ctx, currentPoint.x, currentPoint.y, currentPoint.x + movement.moveX * stepDistance * Math.pow(0.93, i), currentPoint.y + movement.moveY * stepDistance * Math.pow(0.93, i));
+        }
 
         currentPoint = newPoint;
     });
@@ -59,15 +71,32 @@ canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 ctx.lineWidth = 3;
 
+const stanceData = [
+    {
+        name: 'Low/Front Stance',
+        koreanName: 'Ap Kubi',
+        colorCode: 'lowStance'
+    }, {
+        name: 'High/Walking Stance',
+        koreanName: 'Ap Seogi',
+        colorCode: 'highStance'
+    }, {
+        name: 'Side/Back Stance',
+        koreanName: 'Dwi Kubi',
+        colorCode: 'sideStance'
+    },
+];
+
 const stanceColorData = {
     lowStance: 'rgba(255,0,0,$alpha)',
-    walkingStance: 'rgba(0,255,0,$alpha)',
+    highStance: 'rgba(0,255,0,$alpha)',
     sideStance: 'rgba(0,0,255,$alpha)',
     default: 'rgba(0,0,0,$alpha)'
 }
 const formData = [
     {
         name: 'Ki-Bon Il Jang',
+        belt: '#ffffff',
         pattern: [
             { moveX: -1, moveY: 0, stance: 'lowStance' },
             { moveX: -1, moveY: 0, stance: 'lowStance' },
@@ -76,7 +105,7 @@ const formData = [
             { moveX: 0, moveY: -1, stance: 'lowStance' },
             { moveX: 0, moveY: -1, stance: 'lowStance' },
             { moveX: 0, moveY: -1, stance: 'lowStance' },
-            { moveX: 0, moveY: -1, stance: 'lowStance' },
+            { moveX: 0, moveY: -1, stance: 'lowStance', kihup: true },
             { moveX: 1, moveY: 0, stance: 'lowStance' },
             { moveX: 1, moveY: 0, stance: 'lowStance' },
             { moveX: -1, moveY: 0, stance: 'lowStance' },
@@ -84,7 +113,7 @@ const formData = [
             { moveX: 0, moveY: 1, stance: 'lowStance' },
             { moveX: 0, moveY: 1, stance: 'lowStance' },
             { moveX: 0, moveY: 1, stance: 'lowStance' },
-            { moveX: 0, moveY: 1, stance: 'lowStance' },
+            { moveX: 0, moveY: 1, stance: 'lowStance', kihup: true },
             { moveX: -1, moveY: 0, stance: 'lowStance' },
             { moveX: -1, moveY: 0, stance: 'lowStance' },
             { moveX: 1, moveY: 0, stance: 'lowStance' },
@@ -92,6 +121,8 @@ const formData = [
         ]
     }, {
         name: 'Ki-Bon Ee Jang',
+        belt: '#ffffff',
+        beltStripe: '#eeee00',
         pattern: [
             { moveX: -1, moveY: 0, stance: 'lowStance' },
             { moveX: -1, moveY: 0, stance: 'lowStance' },
@@ -100,7 +131,7 @@ const formData = [
             { moveX: 0, moveY: -1, stance: 'lowStance' },
             { moveX: 0, moveY: -1, stance: 'lowStance' },
             { moveX: 0, moveY: -1, stance: 'lowStance' },
-            { moveX: 0, moveY: -1, stance: 'lowStance' },
+            { moveX: 0, moveY: -1, stance: 'lowStance', kihup: true },
             { moveX: 1, moveY: 0, stance: 'lowStance' },
             { moveX: 1, moveY: 0, stance: 'lowStance' },
             { moveX: -1, moveY: 0, stance: 'lowStance' },
@@ -108,7 +139,7 @@ const formData = [
             { moveX: 0, moveY: 1, stance: 'lowStance' },
             { moveX: 0, moveY: 1, stance: 'lowStance' },
             { moveX: 0, moveY: 1, stance: 'lowStance' },
-            { moveX: 0, moveY: 1, stance: 'lowStance' },
+            { moveX: 0, moveY: 1, stance: 'lowStance', kihup: true },
             { moveX: -1, moveY: 0, stance: 'lowStance' },
             { moveX: -1, moveY: 0, stance: 'lowStance' },
             { moveX: 1, moveY: 0, stance: 'lowStance' },
@@ -116,47 +147,50 @@ const formData = [
         ]
     }, {
         name: 'Taegeuk Il Jang',
+        belt: '#eeee00',
         pattern: [
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 0, moveY: -1, stance: 'lowStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 0, moveY: -1, stance: 'lowStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
+            { moveX: 0, moveY: -1, stance: 'lowStance', multiMove: 2 },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: 0, moveY: -1, stance: 'lowStance', multiMove: 4 },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
             { moveX: 0, moveY: 1, stance: 'lowStance' },
-            { moveX: 0, moveY: 1, stance: 'lowStance' },
+            { moveX: 0, moveY: 1, stance: 'lowStance', kihup: true },
         ]
     }, {
         name: 'Koryo',
+        belt: '#000000',
         pattern: [
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
             { moveX: 0, moveY: -1, stance: 'lowStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
             { moveX: 0, moveY: -1, stance: 'lowStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: -1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
-            { moveX: 1, moveY: 0, stance: 'walkingStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: -1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
+            { moveX: 1, moveY: 0, stance: 'highStance' },
             { moveX: 0, moveY: 1, stance: 'lowStance' },
             { moveX: 0, moveY: 1, stance: 'lowStance' },
         ]
     },
 ];
 
+let showLegend = false;
 let currentStep = 0;
 let selectedForm = formData[0];
 draw_form(selectedForm);
@@ -168,19 +202,46 @@ window.onresize = () => {
     draw_form(selectedForm);
 };
 
+// Construct buttons for each form in nav bar
 formData.forEach(form => {
+    let navBtn = document.createElement('div');
+    let span = document.createElement('span');
     let div = document.createElement('div');
-    div.innerHTML = form.name;
-    div.className = 'nav-button';
-    div.onclick = () => {
+
+    span.innerHTML = form.name;
+
+    div.className = 'belt-indicator';
+    div.style.borderTop = '4px solid ' + form.belt;
+    div.style.borderBottom = '4px solid ' + form.belt;
+    div.style.backgroundColor = form.beltStripe || form.belt;
+
+    navBtn.className = 'nav-button';
+    navBtn.onclick = () => {
         currentStep = 0;
         selectedForm = form;
         draw_form(selectedForm);
     };
-    document.getElementById('forms-nav').appendChild(div);
+    navBtn.appendChild(div);
+    navBtn.appendChild(span);
+    document.getElementById('forms-nav').appendChild(navBtn);
 });
 
-// TODO
+// Construct legend from color data
+stanceData.forEach(stance => {
+    let colorDiv = document.createElement('div');
+    let nameSpan = document.createElement('span');
+
+    colorDiv.className = 'legend-color-indicator';
+    colorDiv.style.backgroundColor = stanceColorData[stance.colorCode].replace('$alpha', '1');
+
+    nameSpan.innerHTML = `${stance.name} (${stance.koreanName})`;
+
+    document.getElementById('legendColorColumn').appendChild(colorDiv);
+    document.getElementById('legendNameColumn').appendChild(nameSpan);
+});
+
+
+// Movement navigation clicking
 document.getElementById('forwardStepBtn').onclick = () => {
     currentStep = (currentStep + 1) % selectedForm.pattern.length;
     draw_form(selectedForm);
@@ -189,3 +250,11 @@ document.getElementById('backStepBtn').onclick = () => {
     currentStep = (currentStep - 1 + selectedForm.pattern.length) % selectedForm.pattern.length;
     draw_form(selectedForm);
 }
+
+// Legend toggle
+document.getElementById('legendBtn').onclick = () => {
+    showLegend = !showLegend;
+    document.getElementById('legendBtn').innerHTML = showLegend ? 'Hide Legend' : 'Show Legend';
+    document.getElementById('legendContainer').style.display = showLegend ? 'flex' : 'none';
+    draw_form(selectedForm);
+};
